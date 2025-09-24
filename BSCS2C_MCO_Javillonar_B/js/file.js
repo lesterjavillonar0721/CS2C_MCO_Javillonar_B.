66c700c85d49c80eacac3a08acfcdad7c4141bdb
lesterjavillonar0721 <lesterjavillonar01@gmail.com>
2025-09-24 11:09:03 +0800

    Add files via upload

---
+++ BSCS2C_MCO_Javillonar_B/js/file.js
@@ -0,0 +1,9 @@
+function submitFooterForm(event) {
+    event.preventDefault(); 
+    alert("Thank you! Your message has been sent.");
+    return false; 
+}
+
+function addToCart() {
+    alert("Item added to your cart!");
+}
